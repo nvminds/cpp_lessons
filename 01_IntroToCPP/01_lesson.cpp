6fc2406f850508f3ee8507aa619288cb620a623f
B <B@DESKTOP-QTNA1NL>
2026-08-31 16:26:26 +0300

    Add project files.

---
+++ 01_IntroToCPP/01_lesson.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	// \n - перехід на новий рядок
+	// \t - табуляція
+	// \\ - \деяка інформація\ 
+	// \' - 'деяка інформація'
+	// \" - "деяка інформація"
+	
+	// "" - немає обмеження по символам
+	// '' - можна написати тількі 1 символ
+
+	/*cout << "Hello World!\n";
+	cout << "Hello World!\t";
+	cout << "Hello World!\'aaaaa\'";
+	cout << "Hello World!" << endl;*/
+
+	cout << "I" << endl;
+	cout << "\tlove" << endl;
+	cout << "\t\tyou" << endl;
+	cout << "\t\t\tC++!" << endl;
+
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+
+	cout << "\t...:::RESUME:::...\t" << endl;
+	cout << " " << endl;
+	cout << "Name:\t\t\t    Bohdan" << endl;
+	cout << "Surname:\t\t    Odynets" << endl;
+	cout << "Last name:\t\t    Mychailovich" << endl;
+	cout << " " << endl;
+	cout << "Date of birth:\t\t    20.11.2010" << endl;
+	cout << "City:\t\t\t    Rivne" << endl;
+	cout << "Age:\t\t\t    15" << endl;
+	cout << " " << endl;
+	cout << "Hobby:\t\t\t    C++" << endl;
+	cout << "::.....................................::" << endl;
+
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+
+	cout << "Every" << endl;
+	cout << "     hunter" << endl;
+	cout << "\t    wants" << endl;
+	cout << "\t\t  to" << endl;
+	cout << "\t\t     know" << endl;
+	cout << "\t\t\t  where" << endl;
+	cout << "\t\t\t\tthe" << endl;
+	cout << "\t\t\t\t    pheasant" << endl;
+	cout << "\t\t\t\t\t     is." << endl;
+
+}
