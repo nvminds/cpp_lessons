b30948f666aef67105db5a13c94eab6d916922dd
B <odynetsitstep@gmail.com>
2026-09-02 18:02:26 +0300

    if/else and switch

---
+++ 03_Operators/03_Operators.cpp
@@ -0,0 +1,218 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+    // operators + - * / = =! == < > <= >= %
+    //Unarni    5      -5 (-)   ++  --
+    //Binarni   + - * / = =! == < > <= >= %
+    //Ternarni  
+
+
+    //int a = 5, b = 4;
+
+    //cout << a + b << endl;
+    //cout << a - b << endl;
+    //cout << a * b << endl;
+    //cout << a / b << endl;
+    //cout << a % b << endl;
+    //cout << 3 % 7 << endl; //3
+    //cout << 8 % 9 << endl; //8
+
+    //Increment ta Decrement
+
+    //int a = 2;
+    //cout << "a = " << a << endl;
+    //a = a + 1;
+    //cout << "a = " << a << endl;
+    //a++; // postfix increment
+    //cout << "a = " << a << endl;
+    //++a; // prefix increment
+    //cout << "a = " << a << endl;
+    //    
+    //--a; // prefix decrement
+    //cout << "a = " << a << endl;
+    //a--; // postfix decrement
+    //cout << "a = " << a << endl;
+
+    //int c = 5;
+    //cout << c++ << endl; //6 -------------------->
+    //cout << ++c << endl; //7 <--------------------
+    //cout << c << endl; //7
+    //
+    //c = c + 12;
+    //cout << c << endl;
+    //c += 12;
+    //c = c - 12;
+    //cout << c << endl;
+    //c -= 12;
+    //c = c * 12;
+    //cout << c << endl;
+    //c *= 12;
+    //c = c / 12;
+    //cout << c << endl;
+    //c /= 12;
+    //c = c % 12;
+    //cout << c << endl;
+    //c %= 12;
+
+    ////zvuzuyche peretvorenya
+    ////1. neyavne peretvorenya
+    //int A = 3.33;
+    //cout << "A = " << A << endl;
+
+    ////rozshityuche peretvorenya
+    //double pi = 3.14;
+    //cout << "PI = " << pi << endl; //3.140000.....01
+
+    //unsigned int salary = 3000000000;
+    //cout << salary << endl;
+
+    ////2. yavne peretvorenya
+    //double num = 37.4;
+    //float num2 = (int)num;
+    //cout << num2 << endl;
+
+    //if (ymova)
+    //{
+    //    //dia1
+    //}
+    //else
+    //{
+    //    //dia2
+    //}
+
+    //float numb1, numb2;
+    //cout << "Enter number 1: " << endl;
+    //cin >> numb1;
+    //cout << "Enter number 2: " << endl;
+    //cin >> numb2;
+    //float res = numb1 / numb2;
+
+    //if (numb2 == 0)
+    //{
+    //    cout << "You can't divide by zero!!" << endl;
+    //}
+    //else
+    //{
+    //    cout << "Result : " << numb1 << " / " << numb2 << " = " << res << endl;
+    //}
+    //
+    //
+    ////ternarniy operator  ?:
+    //(numb2 == 0) ? cout << "Error" << endl : cout << "Result = " << res << endl;
+
+
+
+    //int day;
+    //cout << "Enter day number : ";
+    //cin >> day;
+    //if (day==1)
+    //{
+    //    cout << "Monday" << endl;
+    //}
+    //else if (day==2)
+    //{
+    //    cout << "Tuesday" << endl;
+    //}
+    //else if (day==3)
+    //{
+    //    cout << "Wednesday" << endl;
+    //}
+    //else if (day==4)
+    //{
+    //    cout << "Thursday" << endl;
+    //}
+    //else if (day==5)
+    //{
+    //    cout << "Friday" << endl;
+    //}
+    //else if (day==6)
+    //{
+    //    cout << "Saturday" << endl;
+    //}
+    //else if (day==7)
+    //{
+    //    cout << "Sunday" << endl;
+    //}
+    //else
+    //{
+    //    cout << "Error number day" << endl;
+    //}
+
+    float a, b, res;
+    cout << "Enter a : "; cin >> a;
+    cout << "Enter b : "; cin >> b;
+    char key;
+    cout << "\tChoose the operation : " << endl;
+    cout << "[+] - add number" << endl;
+    cout << "[-] - sub number" << endl;
+    cout << "[*] - multy number" << endl;
+    cout << "[/] - div number" << endl;
+    cin >> key;
+    if (key == '+')
+    {
+        res = a + b;
+        cout << "Result = " << res << endl;
+    }
+    else if (key=='-')
+    {
+        res = a - b;
+        cout << "Result = " << res << endl;
+    }
+    else if (key=='*')
+    {
+        res = a * b;
+        cout << "Result = " << res << endl;
+    }
+    else if (key=='/')
+    {
+        res = a / b;
+        cout << "Result = " << res << endl;
+    }
+    else
+    {
+        cout << "Error choice" << endl;
+    }
+
+    switch (key)
+    {
+    default:
+        cout << "Error choice" << endl;
+        break;
+    case '+':
+        res = a + b;
+        cout << "Result = " << res << endl;
+        break;
+    case '-':
+        res = a - b;
+        cout << "Result = " << res << endl;
+        break;
+    case '*':
+        res = a * b;
+        cout << "Result = " << res << endl;
+        break;
+    case '/':
+        res = a / b;
+        cout << "Result = " << res << endl;
+        break;
+    }
+    int day = 5; 
+    cout << "Enter number day : "; cin >> day;
+    switch (day)
+    {
+    case 1: case 2: case 3: case 4: case 5:
+        cout << "Working day" << endl;
+    default:
+        break;
+    }
+
+
+
+
+
+
+
+}
+
+
---
+++ 03_Prac/03_Prac.cpp
@@ -0,0 +1,175 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+    //1 if else
+    //int country;
+    //cout << "Country list :" << endl;
+    //cout << "1. Ukraine\n2. France\n3. Italy\n4. UK\n5. Poland" << endl;
+    //cout << "Enter country number : "; cin >> country;
+    //if (country == 1)
+    //{
+    //    cout << "Ukraine capital is Kiyv" << endl;
+    //}
+    //else if (country == 2)
+    //{
+    //    cout << "France capital is Paris" << endl;
+    //}
+    //else if (country == 3)
+    //{
+    //    cout << "Italy capital is Rome" << endl;
+    //}
+    //else if (country == 4)
+    //{
+    //    cout << "United Kingdom capital is London" << endl;
+    //}
+    //else if (country == 5)
+    //{
+    //    cout << "Poland capital is Warshava" << endl;
+    //}
+    //else
+    //{
+    //    cout << "Error country number" << endl;
+    //}
+
+    //1 switch
+    //cout << "Country list :" << endl;
+    //cout << "1. Ukraine\n2. France\n3. Italy\n4. UK\n5. Poland" << endl;
+    //cout << "Enter country number : "; cin >> country;
+    //switch (country)
+    //{
+    //case 1:
+    //    cout << "Ukraine capital is Kiyv" << endl;
+    //    break;
+    //case 2:
+    //    cout << "France capital is Paris" << endl;
+    //    break;
+    //case 3:
+    //    cout << "Italy capital is Rome" << endl;
+    //    break;
+    //case 4:
+    //    cout << "United Kingdom capital is London" << endl;
+    //    break;
+    //case 5:
+    //    cout << "Poland capital is Warshava" << endl;
+    //    break;
+    //default:
+    //    break;
+    //}
+
+
+    //2 if else
+    //int day;
+    //cout << "Enter day number : ";
+    //cin >> day;
+    //if (day==1 or day==2 or day==3 or day==4 or day==5)
+    //{
+    //    cout << "Working day" << endl;
+    //}
+    //else if (day==6 or day==7)
+    //{
+    //    cout << "Weekday" << endl;
+    //}
+    //else
+    //{
+    //    cout << "Error number day!" << endl;
+    //}
+
+    //2 switch
+    //int day;
+    //cout << "Enter day number : ";
+    //cin >> day;
+    //switch (day)
+    //{
+    //case 1: case 2: case 3: case 4: case 5:
+    //    cout << "Working day" << endl;
+    //    break;
+    //case 6: case 7:
+    //    cout << "Weekday" << endl;
+    //    break;
+    //default:
+    //    break;
+    //}
+
+
+    //3 if else
+    //char direction;
+    //cout << "N - North\nS - South\nW - West\nE - East" << endl;
+    //cout << "Enter ship direction : "; cin >> direction;
+    //if (direction=='N')
+    //{
+    //    cout << "North reverse direction - South" << endl;
+    //}
+    //else if (direction=='S')
+    //{
+    //    cout << "South reverse direction - North" << endl;
+    //}
+    //else if (direction=='W')
+    //{
+    //    cout << "West reverse direction - East" << endl;
+    //}
+    //else if (direction=='E')
+    //{
+    //    cout << "East reverse direction - West" << endl;
+    //}
+    //else
+    //{
+    //    cout << "Error: Unknown direction" << endl;
+    //}
+
+    //3 switch
+    //char direction;
+    //cout << "N - North\nS - South\nW - West\nE - East" << endl;
+    //cout << "Enter ship direction : "; cin >> direction;
+    //switch (direction)
+    //{
+    //case 'N':
+    //    cout << "North reverse direction - South" << endl;
+    //    break;
+    //case 'S':
+    //    cout << "South reverse direction - North" << endl;
+    //    break;
+    //case 'W':
+    //    cout << "West reverse direction - East" << endl;
+    //    break;
+    //case 'E':
+    //    cout << "East reverse direction - West" << endl;
+    //    break;
+    //default:
+    //    break;
+    //}
+
+    
+    //4 if else
+    //int animal;
+    //cout << "1. Giraffe\n2. Tiger\n3. Crocodile\n4. Rabbit\n5. Lion\n6. Wolf\n7. Cow" << endl;
+    //cout << "Enter animal number : "; cin >> animal;
+    //if (animal==2 or animal==3 or animal==5 or animal==6)
+    //{
+    //    cout << "This is carnivorous animal" << endl;
+    //}
+    //else if (animal==1 or animal==4 or animal==7)
+    //{
+    //    cout << "This is herbivorous animal" << endl;
+    //}
+    //else{
+    //    cout << "Error animal number" << endl;
+    //}
+
+    //4 switch
+    //int animal;
+    //cout << "1. Giraffe\n2. Tiger\n3. Crocodile\n4. Rabbit\n5. Lion\n6. Wolf\n7. Cow" << endl;
+    //cout << "Enter animal number : "; cin >> animal;
+    //switch (animal)
+    //{
+    //case 1: case 4: case 7:
+    //    cout << "This is herbivorous animal" << endl;
+    //    break;
+    //case 2: case 3: case 5: case 6:
+    //    cout << "This is carnivorous animal" << endl;
+    //    break;
+    //default:
+    //    break;
+    //}
+}
