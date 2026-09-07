ba81b59651eb875f3433814f5193f2b34ccdef22
B <odynetsitstep@gmail.com>
2026-09-07 17:16:13 +0300

    include cycles

---
+++ 05_IncludeCycles/05_IncludeCycles.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 1; j <= 5; j++)
+		{
+		cout << "|###|";
+		}
+			cout << endl;
+	}
+
+
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 1; j <= 10; j++)
+		{
+			cout << i << " * " << j << " = " << (i * j) << endl;
+		}
+		cout << endl;
+	}
+
+
+	int star_row = 1;
+	int star_column;
+	int lenght = 10;
+	while (star_row <= lenght)
+	{
+		int star_column = 1;
+		while (star_column <= lenght)
+		{
+			cout << "* ";
+			star_column++;
+		}
+		cout << endl;
+		star_row++;
+
+	}
+
+	cout << endl;
+
+	for (int i = 1; i <= 3; i++)
+	{
+		for (int j = 1; j <= 4; j++)
+		{
+			cout << " +";
+		} cout << endl;
+	}
+
+	cout << endl;
+
+	for (int i = 1; i <= 20; i++)
+	{
+		for (int j = 1; j <= 20; j++)
+		{
+			if (i == j) {
+				cout << " \\";
+			}
+			else if (i+j==22-1){
+				cout << " /";
+			}
+			else {
+
+				cout << " +";
+			}
+		} cout << endl;
+	}
+
+	for (int i = 0; i < 11; i++)
+	{
+		for (int j = 0; j < 11; j++)
+		{
+			if (i > j and i+j>10-1 ) {
+				cout << "|===|";
+			}
+			else {
+				cout << "     ";
+			}
+		}
+		cout << endl;
+	}
+
+	for (int i =0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << "|###|";
+		}
+		cout << endl;
+	}
+
+
+
+}
---
+++ 05_Prac/05_Prac.cpp
@@ -0,0 +1,195 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " ==========1=========== \n" << endl;
+	//1
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (i==j or i<j){
+				
+				cout << "::";
+			}
+			else {
+
+				cout << "##";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	cout << " ==========2=========== \n" << endl;
+	//2
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (i == j or i > j) {
+
+				cout << "::";
+			}
+			else {
+
+				cout << "##";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	cout << " ==========3=========== \n" << endl;
+	//3
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (i > j or i + j > 12 - 1) {
+
+				cout << "::";
+			}
+			else {
+
+				cout << "##";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	cout << " ==========4=========== \n" << endl;
+	//4
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (i < j or i + j < 12 - 1) {
+
+				cout << "::";
+			}
+			else {
+
+				cout << "##";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	cout << " ==========5=========== \n" << endl;
+	//5
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (i > j and i + j > 12 - 1 or i < j and i + j < 12 - 1 or i==j or i+j==12-1) {
+
+				cout << "::";
+			}
+			else {
+
+				cout << "##";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	cout << " ==========6=========== \n" << endl;
+	//6
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (i > j and i + j < 12 - 1 or i < j and i + j > 12 - 1 or i==j or i+j==12-1) {
+
+				cout << "::";
+			}
+			else {
+
+				cout << "##";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	cout << " ==========7=========== \n" << endl;
+	//7
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (i > j and i + j < 12 - 1) {
+
+				cout << "::";
+			}
+			else {
+
+				cout << "##";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	cout << " ==========8=========== \n" << endl;
+	//8
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (i < j and i + j > 12 - 1) {
+
+				cout << "::";
+			}
+			else {
+
+				cout << "##";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	cout << " ==========9=========== \n" << endl;
+	//9
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (i + j==12-1 or i + j < 12-1) {
+
+				cout << "::";
+			}
+			else {
+
+				cout << "##";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	cout << " ==========10========== \n" << endl;
+	//10
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			if (i + j == 12 - 1 or i + j > 12 - 1) {
+
+				cout << "::";
+			}
+			else {
+
+				cout << "##";
+			}
+		}
+		cout << endl;
+	}
+}
+
