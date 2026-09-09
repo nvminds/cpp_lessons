76138c72b095ee1603a4ef1096a5d14441e14c0a
B <odynetsitstep@gmail.com>
2026-09-09 18:05:59 +0300

    arrays

---
+++ 06_Arrays/06_Arrays.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    //масив це набір однотипних даних, об'єднаний загальним ім'ям
+
+    //int train[3];
+    //train[0] = 33;
+    //train[1] = 1;
+    //train[2] = 10;
+    //cout << "vagon 1 : " << train[0] << endl;
+    //cout << "vagon 2 : " << train[1] << endl;
+    //cout << "vagon 3 : " << train[2] << endl;
+
+    //const int size = 12;
+    //int marks[size];
+    //marks[0] = 10;
+    //marks[1] = 4;
+    //marks[2] = 12;
+    //marks[3] = 7;
+    //marks[4] = 8;
+    //marks[5] = 8;
+    //marks[6] = 9;
+    //marks[7] = 11;
+    //marks[8] = 10;
+    //marks[9] = 5;
+    //marks[10] = 3;
+    //marks[11] = 7;
+    //cout << "marks : " << marks[0] << " - " << marks[5] << " - " << marks[11] << endl;
+
+    //int arr1[5];
+
+    //const int size = 10;
+    //double arr[size];
+
+    const int size = 10;
+    int arr1[size] = { 1 };
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
+
+    int arr2[size] = {};
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+
+    int arr3[size] = {1,2,3};
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr3[i] << " ";
+    }
+    cout << endl;
+
+    //int arr4[size];
+    //for (int i = 0; i < size; i++)
+    //{
+    //    cout << "Enter number " << i+1 << " : "; cin >> arr4[i];
+    //}
+    //for (int i = 0; i < size; i++)
+    //{
+    //    cout << arr4[i] << " ";
+    //}
+
+    //1
+    const int size1 = 10;
+    int arr5[size1] = { 5, -9, 11, -4, 22, 7, -6, 2, 3, 8 };
+    int summ = 0;
+    int min = arr5[0], max = arr5[0];
+
+    for (int i = 0; i < size1; i++)
+    {
+        if (arr5[i]<0)
+        {
+            summ += arr5[i];
+        }
+        if (arr5[i] > max) {
+            max =arr5[i];
+        }
+        if (arr5[i] < min) {
+            min = arr5[i];
+        }
+    }
+    cout << "Negative elements : " << summ << endl;
+    cout << "Max element : " << max << endl;
+    cout << "Min element : " << min << endl;
+
+}
---
+++ 06_Prac/06_Prac.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    ////1
+    //const int size = 10;
+    //int arr[size];
+    //int dob = 1;
+    //for (int i = 0; i < size; i++)
+    //{
+    //    cout << "Enter number : "; cin >> arr[i];
+    //    dob *= arr[i];
+    //}
+    //cout << "Multy all elements : " << dob << endl;
+
+    //2
+    //const int size = 7;
+    //int arr[size] = {-12, 50, 21, 75, -33, 19, -45};
+    //int summ_plus = 0, summ_minus = 0; 
+    //for (int i = 0; i < size; i++)
+    //{
+    //    if (arr[i]>0){
+    //        summ_plus += arr[i];
+    //    }
+    //    if (arr[i]<0){
+    //        summ_minus += arr[i];
+    //    }
+    //}
+    //cout << "Summ plus : " << summ_plus << endl;
+    //cout << "Minus plus : " << summ_minus << endl;
+
+    //3
+    //const int size = 7;
+    //int arr[size] = { 1, 2, 3, 4, 5, 6, 7 };
+    //int summ = 0;
+    //for (int i = 0; i < size; i++)
+    //{
+    //    if (arr[i] % 2 == 0) {
+    //        summ += arr[i];
+    //    }
+    //}
+    //cout << "Summ of even numbers : " << summ << endl;
+
+    //4
+    //const int size = 10;
+    //int arr[size];
+    //int quarter;
+    //for (int i = 1; i < size; i++)
+    //{
+    //    arr[i] = 2 * i;
+
+    //    cout << arr[i] << " ";
+    //    
+    //}
+    //cout << endl;
+    //cout << endl;
+    //for (int i = size-1; i > 0; i--)
+    //{
+    //    arr[i] = 2 * i;
+
+    //    cout << arr[i] << " ";
+    //    
+    //}
+
+    //5
+    //const int size = 10;
+    //int arr[size] = { -12, 50, 21, 75, -33, 19, -45, 10, -67, 32 };
+
+    //for (int i = 0; i < size; i++)
+    //{
+    //    if (arr[i] < 0) {
+    //        arr[i] *= -1;
+    //    }
+    //    cout << arr[i] << " ";
+    //}
+
+    //6
+    const int size = 12;
+    int arr[size];
+    int diap1, diap2, min=0, max = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter company salary in " << i+1 << " month : "; cin >> arr[i];
+    }
+
+    cout << endl;
+    cout << "Enter 1st diapazone index : "; cin >> diap1;
+    cout << "Enter 2nd diapazone index : "; cin >> diap2;
+    diap1--;
+    diap2--;
+
+    for (int i = diap1; i <= diap2; i++)
+    {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < max) {
+            min = arr[i];
+        }
+    }
+
+    cout << endl;
+    cout << "Salary in " << diap1+1 << "-" << diap2+1 << " diapazone : " << endl;
+    cout << "Min salary : " << min << endl;
+    cout << "Max salary : " << max << endl;
+    
+}
