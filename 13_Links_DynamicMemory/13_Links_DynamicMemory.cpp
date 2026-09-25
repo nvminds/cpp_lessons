d636814e96dfdcf37f0e8aeda456a1f0e46d9a28
B <odynetsitstep@gmail.com>
2026-09-25 18:16:32 +0300

    links & dynamic memory (heap)

---
+++ 13_Links_DynamicMemory/13_Links_DynamicMemory.cpp
@@ -0,0 +1,150 @@
+﻿#include <iostream>
+#include <conio.h>
+using  namespace std;
+
+void Change(int& a)//adress
+{
+    a++;
+}
+int& findMax(int& a, int& b)
+{
+    if (a > b) return a;
+    else return b;
+}
+void Test1()
+{
+    const int size = 10;
+    int arr[size];
+}
+void Test2()
+{
+    int size = 10;
+    cin >> size;
+    int *arr = new int[size];
+    delete[]arr;
+}
+int* CreateArr(int size)
+{
+    int* arr = new int[size];
+    return arr;
+}
+void InitArray(int* arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 100;
+    }cout << endl;
+}
+void ShowArray(int* arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }cout << endl;
+}
+int* addNewNumber(int *arr, int* size, int number)
+{
+    int* temp = new int[*size + 1];
+    for (int i = 0; i < *size; i++)
+    {
+        temp[i] = arr[i];
+    }
+    temp[*size] = number;
+    delete[]arr;
+    (*size)++;
+    return temp;
+}
+
+
+int main()
+{
+    srand(time(0));
+
+    int size = 3;
+    //cout << "Enter size : "; cin >> size;
+    int* arr = CreateArr(size);
+    InitArray(arr, size);
+    ShowArray(arr, size);
+
+    int number;
+    char choice = 'y';
+    while (true)
+    {
+        cout << "Do you want add number ? y/n..." << endl;
+        choice = _getch();
+        if (choice == 'n') break;
+        else {
+            cout << "Enter number : "; cin >> number;
+            arr = addNewNumber(arr, &size, number);
+            //system("cls");
+            ShowArray(arr, size);
+        }
+    }
+
+    delete[]arr;
+
+    //int a = 10; // stack memory
+
+    //int* pa = new int(10); // heap memory
+    //int* pb = new int;
+    //int* pc = nullptr;
+
+    //*pb = 5;
+    //pc = new int(12);
+
+    //cout << "pa = " << pa << endl;
+    //cout << "pb = " << pb << endl;
+    //cout << "pc = " << pc << endl;
+    //cout << endl;
+    //cout << "*pa = " << *pa << endl;
+    //cout << "*pb = " << *pb << endl; // return trash
+    //cout << "*pc = " << *pc << endl; 
+
+    //delete pc;
+    //pc = new int(55);
+    //cout << endl;
+    //cout << "*pa = " << *pa << endl;
+    //cout << "*pb = " << *pb << endl; // return trash
+    //cout << "*pc = " << *pc << endl; 
+
+
+
+
+
+
+
+
+    //delete pa;
+    //delete pb;
+    //delete pc;
+    
+    /*
+    // pointers  &  links
+    int a = 10; // variable
+    int* pa = &a; // pointer
+    int& la = a; // link
+    cout <<"a = " << a << endl;
+    cout <<"pa = " << pa << endl;
+    cout <<"*pa = " << *pa << endl;
+    cout <<"la = " << la << endl;
+
+    int b = 7;
+    //int* pb;
+    int* pb = nullptr;
+    pb = &b;
+    //int& lb; !error
+    int& lb = b; 
+    cout << "lb = " << lb << endl;
+    cout << "lb = " << lb << endl;
+    cout << "a = " << a << endl;
+    Change(a);
+    cout << "a = " << a << endl;
+    cout << endl;
+
+    findMax(a, b) = 25;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "Max el : " << findMax(a, b) << endl;
+    */
+}
+
---
+++ 13_Prac/13_Prac.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+using namespace std;
+
+int* CreateArr(int size)
+{
+    int* arr = new int[size];
+    return arr;
+}
+void InitArray(int* arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 100;
+    }cout << endl;
+}
+void ShowArray(int* arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }cout << endl;
+}
+int* removeLastNumber(int* arr, int* size)
+{
+    int* temp = new int[*size - 1];
+    for (int i = 0; i < *size-1; i++)
+    {
+        temp[i] = arr[i];
+    }
+    delete[]arr;
+    (*size)--;
+    return temp;
+}
+int* addNumberToEnd(int* arr, int* size, int number)
+{
+    int* temp = new int[*size + 1];
+    for (int i = 0; i < *size; i++)
+    {
+        temp[i] = arr[i];
+    }
+    temp[*size] = number;
+    delete[]arr;
+    (*size)++;
+    return temp;
+}
+int* addNumberToPos(int* arr, int* size, int number, int pos)
+{
+    int* temp = new int[*size + 1];
+    for (int i = 0; i < pos; i++)
+    {
+        temp[i] = arr[i];
+    }
+    temp[pos] = number;
+    for (int i = pos; i < *size; i++)
+    {
+        temp[i+1] = arr[i];
+    }
+    delete[]arr;
+    (*size)++;
+    return temp;
+}
+int* removeNumberByPos(int* arr, int* size, int pos)
+{
+    int* temp = new int[*size - 1];
+    for (int i = 0; i < pos; i++)
+    {
+        temp[i] = arr[i];
+    }
+    for (int i = pos+1; i < *size; i++)
+    {
+        temp[i-1] = arr[i];
+    }
+    delete[]arr;
+    (*size)--;
+    return temp;
+}
+
+
+int main()
+{
+    srand(time(0));
+
+    //1
+    //int* pa = new int(10);
+    //float* pb = new float(6.7);
+    //double* pc = new double(13.37);
+
+    //cout << "pa = " << pa << endl;
+    //cout << "pb = " << pb << endl;
+    //cout << "pc = " << pc << endl;
+    //cout << endl;
+    //cout << "*pa = " << *pa << endl;
+    //cout << "*pb = " << *pb << endl; 
+    //cout << "*pc = " << *pc << endl; 
+    //cout << endl;
+    //cout << "(*pa) * (*pb) * (*pc) = " << (*pa) * (*pb) * (*pc) << endl;
+
+    //delete pa;
+    //delete pb;
+    //delete pc;
+
+    //2
+    int size=1;
+    int number, pos;
+    cout << "Enter array size : "; cin >> size;
+    int* arr = CreateArr(size);
+    InitArray(arr, size);
+    cout << "Default array : "; ShowArray(arr, size); cout << endl;
+    
+
+    cout << "Enter number add to end : "; cin >> number;
+    arr = addNumberToEnd(arr, &size, number);
+    cout << "Array with added number to end : "; ShowArray(arr, size);
+
+    cout << "Enter number position to add : "; cin >> pos;
+    cout << "Enter number : "; cin >> number;
+    arr = addNumberToPos(arr, &size, number, pos);
+    cout << "Array with added number to pos : "; ShowArray(arr, size);
+    
+    cout << "Enter number position to remove : "; cin >> pos;
+    arr = removeNumberByPos(arr, &size, pos);
+    cout << "Array with removed number from pos : "; ShowArray(arr, size);
+
+    arr = removeLastNumber(arr, &size);
+    cout << "Array with deleted last : "; ShowArray(arr, size);
+
+
+    delete[]arr;
+}
